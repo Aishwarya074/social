5f99a90910c67156600e001401f39fd1823b5ac6
Aishwarya074 <aishwaryabharsawade07042004@gmail.com>
2025-08-14 13:39:41 +0530

    abc

--- front/register.php
+++ front/register.php
@@ -131,12 +131,12 @@
 </footer>
 
 
-<!-- Bootstrap JS --
+<!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
 
--->
+
 
 
 <!--
